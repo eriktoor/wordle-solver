1736a82a713be8a4bc16b4bd378c88aa9ba60461
Erik Toor <eriktoor@gmail.com>
2022-02-10 11:41:57 -0500

    Update wordle_against_you.py

--- wordle_against_you.py
+++ wordle_against_you.py
@@ -2,7 +2,8 @@
 
 import random 
 
-WORD_SIZE = 5 
+from wordle_utils import all_letter_dictionaries, one_letter_dictionary, get_all_words, WORD_SIZE, get_possible_words
+
 CORRECT_WORD = input("What word would you like this system to solve for? ").lower().strip()
 
 def guess_word(words, guess, letters_not_in, letters_in_wrong_place, letters_in_right_place):
@@ -11,122 +12,11 @@ def guess_word(words, guess, letters_not_in, letters_in_wrong_place, letters_in_
         if CORRECT_WORD[idx] == guess[idx]:
             letters_in_right_place[idx] = CORRECT_WORD[idx]
         elif guess[idx] in CORRECT_WORD: 
-            letters_in_wrong_place.append((guess[idx], idx))
+            letters_in_wrong_place[idx].append(guess[idx])
         else: 
             letters_not_in.append(guess[idx])
 
     return letters_not_in, letters_in_wrong_place, letters_in_right_place
-
-
-def remove_letters_not_in_word(words, letters_not_in_word):
-
-    end = len(words)-1
-    curr = 0 
-    increment = True 
-
-    while curr < end: 
-        word = words[curr]
-        for t in letters_not_in_word: 
-            if t in word: 
-                words[curr], words[end] = words[end], words[curr] # move word to end  
-                end -= 1 
-                increment = False 
-                break
-        if increment: 
-            curr += 1 
-        increment = True        
-
-    for t in letters_not_in_word: 
-        if t in words[end]: 
-            return words[:end]
-
-    return words[:end+1]
-
-def remove_words_with_letters_in_wrong_places(words, letters_in_wrong_place):
-
-    end = len(words)-1
-    curr = 0 
-    increment = True 
-
-    while curr <= end: 
-        word = words[curr]
-        for letter, place in letters_in_wrong_place: 
-            if letter == word[place]: 
-                words[curr], words[end] = words[end], words[curr] # move word to end  
-                end -= 1 
-                increment = False 
-                break
-        if increment: 
-            curr += 1 
-        increment = True        
-
-    return words[:end+1]
-
-
-def remove_words_with_letters_not_in_right_place(words, letters_in_right_place):
-
-    end = len(words)-1
-    curr = 0 
-    increment = True 
-
-    while curr <= end: 
-        word = words[curr]
-        for place, letter in enumerate(letters_in_right_place): 
-            if letter == "":
-                continue 
-            if letter != word[place]: 
-                words[curr], words[end] = words[end], words[curr] # move word to end  
-                end -= 1 
-                increment = False 
-                break
-        if increment: 
-            curr += 1 
-        increment = True        
-
-    return words[:end+1]
-
-def get_all_words(): 
-    with open('wordlist.txt') as f:
-        file = f.read()
-        lines = file.split("\n")
-
-    f = open("demofile3.txt", "w")
-    f.write(str(lines))
-    f.close()
-    return lines 
-
-def all_letter_dictionaries(words):
-    letter_1, letter_2, letter_3, letter_4, letter_5 = {}, {}, {}, {}, {}
-
-    for word in words: 
-        if WORD_SIZE != len(word): raise("THIS IS A PROBLEM")
-
-        if word[0] not in letter_1:
-            letter_1[word[0]] = [word]
-        else: 
-            letter_1[word[0]].append(word)
-
-        if word[1] not in letter_2:
-            letter_2[word[1]] = [word]
-        else: 
-            letter_2[word[1]].append(word)
-        
-        if word[2] not in letter_3:
-            letter_3[word[2]] = [word]
-        else: 
-            letter_3[word[2]].append(word)
-
-        if word[3] not in letter_4:
-            letter_4[word[3]] = [word]
-        else: 
-            letter_4[word[3]].append(word)
-
-        if word[4] not in letter_5:
-            letter_5[word[4]] = [word]
-        else: 
-            letter_5[word[4]].append(word)
-
-        return letter_1, letter_2, letter_3, letter_4, letter_5
 
 def main(): 
     # STEP 1: READ THE FILE AND TURN IT INTO A WORDSLSIT 
@@ -134,6 +24,7 @@ def main():
 
     # STEP 2: Make dictionaries for each letter placement 
     l1, l2, l3, l4, l5 = all_letter_dictionaries(words)
+    hmap = one_letter_dictionary(words)
 
     letters_in_right_place = ["", "", "", "", ""]
 
@@ -141,7 +32,7 @@ def main():
     for guess in range(6):
 
         letters_not_in_word = []
-        letters_in_wrong_place = []
+        letters_in_wrong_place = [[],[],[],[],[]]
 
         word_to_guess = random.choice(words)
 
@@ -154,10 +45,7 @@ def main():
 
         letters_not_in_word, letters_in_wrong_place, letters_in_right_place  = guess_word(words, word_to_guess, letters_not_in_word, letters_in_wrong_place, letters_in_right_place)
 
-        words = remove_letters_not_in_word(words, letters_not_in_word)
-        words = remove_words_with_letters_in_wrong_places(words, letters_in_wrong_place)
-        words = remove_words_with_letters_not_in_right_place(words, letters_in_right_place)
-        
+        words = get_possible_words(words, letters_not_in_word, letters_in_wrong_place, letters_in_right_place)
 
         print("Letters not in the word: ", letters_not_in_word)
         print("Letters in the word but in the wrong place: ", letters_in_wrong_place)
